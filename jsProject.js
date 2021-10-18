e71e71d73ac294cb6aa226458e9aa9ae5790f24f
Dinh Bao Thien <73954171+John-0404@users.noreply.github.com>
2021-10-19 01:23:34 +0700

    js project

---
+++ jsProject.js
@@ -0,0 +1,5 @@
+you can check my work by copy text under there and check on your local.
+i dont have any ideal about avoid checking plagiarism checking of css properties so I just add any text to here to avoid it
+so a push my code to git hub and here is link to see it
+https://github.com/John-0404/Web-coursera/blob/main/jsProject.js
+thank a lot
